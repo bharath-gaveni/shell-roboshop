620f321cd6f3679dff6676f62c86e410dd48c3ee
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-28 23:33:30 +0530

    practice

--- catalogue.sh
+++ catalogue.sh
@@ -81,8 +81,8 @@ validate $? "copying the mongo.repo"
 dnf install mongodb-mongosh -y &>>$log_file
 validate $? "installing mongodb cilent to connect with mongodb DB"
 
-index=$(mongosh bharathgaveni.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ "$index" -le 0 ]; then
+index=$(mongosh mongodb.bharathgaveni.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $index -le 0 ]; then
 mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
 else
     echo "Database is already loaded with Catalogue DB products"
