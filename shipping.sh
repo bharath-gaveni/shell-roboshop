107e80904ce6d02abd0195a77f306f927f4056e5
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-29 02:18:20 +0530

    practice

--- shipping.sh
+++ shipping.sh
@@ -79,11 +79,11 @@ validate $? "started the shipping"
 dnf install mysql -y &>>$log_file
 validate $? "installing mysql client to load data to mysql DB"
 
-mysql -h $Host_name -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
+mysql -h $Host_name -uroot -pRoboShop@1 -e 'use cities' &>>$log_file
 if [ $? -ne 0 ]; then
-    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
+    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$log_file
+    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
 else
     echo "Shipping data is already loaded"
 fi       
